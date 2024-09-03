d9e10bc0409a53fbdbc609ca691e36975a11b387
MaVeN-13TTN <kinyanjuindungu1324@gmail.com>
2024-09-03 20:41:42 +0300

    update the profile image

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,11 +1,11 @@
-import profilePic from "../assets/me.jpeg";
+import profilePic from "../assets/1324.jpg";
 
 function Card() {
   return (
     <div className="relative inline-block">
-      <div className="absolute inset-0 bg-purple-600 rounded-full blur-xl opacity-75"></div>
+      <div className="absolute inset-0 bg-purple-600 rounded-2xl blur-xl opacity-75"></div>
       <img
-        className="relative w-64 h-64 rounded-full object-cover border-4 border-princeton-orange"
+        className="relative w-80 h-80 rounded-2xl object-cover border-4 border-princeton-orange"
         src={profilePic}
         alt="Ndung'u Kinyanjui"
       />
