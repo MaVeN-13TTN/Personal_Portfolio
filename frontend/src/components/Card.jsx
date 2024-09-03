4c7d233463e01e4ef77616838f6ec3ee0b4f019c
MaVeN-13TTN <kinyanjuindungu1324@gmail.com>
2024-09-03 21:00:20 +0300

    new cloud-computing image

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,13 +1,13 @@
-import profilePic from "../assets/1324.jpg";
+import profilePic from "../assets/cloud-computing.svg";
 
 function Card() {
   return (
     <div className="relative inline-block">
       <div className="absolute inset-0 bg-purple-600 rounded-2xl blur-xl opacity-75"></div>
       <img
-        className="relative w-80 h-80 rounded-2xl object-cover border-4 border-princeton-orange"
+        className="relative w-96 h-96 rounded-2xl object-cover border-4 border-princeton-orange"
         src={profilePic}
-        alt="Ndung'u Kinyanjui"
+        alt="Cloud Computing"
       />
     </div>
   );
